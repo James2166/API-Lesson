5abd188f56f39346cc4e941cad4f5454b3df8fd4
James2166 <jlh3ndr@gmail.com>
2022-02-28 14:12:56 -0700

    Finished Project

---
+++ script.js
@@ -0,0 +1,79 @@
+const cardsContainer = document.querySelector('.cards')
+// const logo = document.getElementById('logo1');
+// const business = document.getElementById('business-name');
+// const description = document.getElementById('description');
+// const address = document.getElementById('address');
+
+let data = [];
+let i = 0;
+
+// function businessCard() {
+//     logo.src = data.business[i].imageurl;
+//     business.textContent = data.business[i].name;
+//     address.textContent = data.business[i].address;
+//     description.textContent = data.business[i].description;
+//     console.log(data.business[i]);
+// };
+
+function createCards() {
+
+    // The Card
+    const card = document.createElement('div');
+    card.classList.add('card')
+    // Logo Container
+    const logo = document.createElement('div');
+    logo.classList.add('logo');
+    // Business Name
+    const title = document.createElement('div');
+    title.classList.add('business-name');
+    //Business Address
+    const adrs = document.createElement('div');
+    adrs.classList.add('business-address');
+    //Business Description
+    const descript = document.createElement('div');
+    descript.classList.add('business-description');
+
+    //Logo Image
+    const image = document.createElement('img');
+    image.src = data.business[i].imageurl;
+    image.alt = 'Business Logo';
+    // Name
+    const name = document.createElement('p');
+    name.textContent = data.business[i].name;
+    // Address
+    const address = document.createElement('p');
+    address.textContent = data.business[i].address;
+    //Description
+    const description = document.createElement('p');
+    description.textContent = data.business[i].description;
+
+    //Append
+    descript.appendChild(description);
+    adrs.appendChild(address);
+    title.appendChild(name);
+    logo.appendChild(image);
+    card.append(logo, title, adrs, descript);
+    cardsContainer.appendChild(card);
+}
+
+function buildAllCards() {
+    for(a = 0; a < data.business.length; a++) {
+        createCards();
+        i++;
+    }
+}
+
+
+async function getData() {
+    const apiUrl = 'https://iskarr.github.io/austindonovan.github.io/api/business.json';
+    try {
+        const response = await fetch(apiUrl);
+        data = await response.json();
+        buildAllCards();
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getData();
